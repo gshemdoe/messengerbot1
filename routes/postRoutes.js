3d9da6caa0f384e4b7b30c7167173b2e1c0e6451
gshemdoe <georgehmariki@gmail.com>
2023-02-24 18:38:48 +0300

    updated to answer messages

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express')
-const handleMessage = require('../functions/handlers')
+const handles = require('../functions/handlers')
 const router = express.Router()
+const request = require('request')
 
 router.post('/webhook', (req, res) => {
 
@@ -20,6 +21,14 @@ router.post('/webhook', (req, res) => {
             // Get the sender PSID
             let sender_psid = webhook_event.sender.id;
             console.log('Sender PSID: ' + sender_psid);
+
+            // Check if the event is a message or postback and
+            // pass the event to the appropriate handler function
+            if (webhook_event.message) {
+                handles.handleMessage(sender_psid, webhook_event.message);
+            } else if (webhook_event.postback) {
+                handles.handlePostback(sender_psid, webhook_event.postback);
+            }
         }
 
         // Return a '200 OK' response to all events
