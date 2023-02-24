fb7ef7549a2e1c050229e81fbd6aa11904d57d8d
gshemdoe <georgehmariki@gmail.com>
2023-02-24 17:09:17 +0300

    Test 2 commit

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 const express = require('express')
+const app = express()
 require('dotenv').config()
+
+const getRoutes = require('./routes/getRoutes')
+const postRoutes = require('./routes/postRoutes')
+
+//middlewares
+app.set('view engine', 'ejs')
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static(__dirname + '/public'))
+app.use(getRoutes)
+app.use(postRoutes)
+
+app.listen(process.env.PORT || 3000, ()=> console.log("Running on port 3000"))
